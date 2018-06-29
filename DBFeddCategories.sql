da9eb76169e8c61d092656d28d59499df7f990ed
cchaldezos <cchaldezos@CPWLT157>
2018-06-29 12:23:27 +0300

    categories feeding script added

---
+++ DBFeddCategories.sql
@@ -0,0 +1,13 @@
+Use FinalProjectNik  --your DBNamne
+Go
+
+INSERT INTO [dbo].[Category] ([Title])
+		VALUES  ('Games'),
+				('Food & Craft'),
+				('Film'),
+				('Music'),
+				('Design & Tech'),
+				('Publishing'),
+				('Arts'),
+				('Comics & Illustration');
+
