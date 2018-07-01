8fc375a738a67b7b4f8ab054ab185113201a8fd8
cchaldezos <cchaldezos@CPWLT157>
2018-07-01 12:20:53 +0300

    categories feed

---
+++ DBFeed.sql
@@ -0,0 +1,9 @@
+INSERT INTO [dbo].[Category] ([CategoryId],[Name])
+		VALUES  (1,'Games'),
+				(2,'Food & Craft'),
+				(3,'Film'),
+				(4,'Music'),
+				(5,'Design & Tech'),
+				(6,'Publishing'),
+				(7,'Arts'),
+				(8,'Comics & Illustration');
